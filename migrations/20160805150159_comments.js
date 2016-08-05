a03c61a33d46b42053b1620c881da50346d0ee4f
Kyle Coberly <kyle.coberly@gmail.com>
2016-08-05 16:36:23 -0600

    Adding comments API

--- migrations/20160805150159_comments.js
+++ migrations/20160805150159_comments.js
@@ -1,10 +1,19 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable("comment", function(comment){
-        comment.increments();
-        comment.string("message");
-    });  
+    return knex.schema.createTable("site", function(site){
+        site.increments();
+        site.string("url");
+    }).then(function(){
+        return knex.schema.createTable("comment", function(comment){
+            comment.increments();
+            comment.string("message");
+            comment.integer("site").references("id").inTable("site");
+        });
+    });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("comment"); 
+    return knex.schema.dropTableIfExists("comment")
+    .then(function(){
+        return knex.schema.dropTableIfExists("site");
+    });
 };
--- routes/comments.js
+++ routes/comments.js
@@ -2,11 +2,36 @@ var express = require('express');
 var router = express.Router();
 var databaseConnection = require("../database_connection");
 
+router.get('/', function(req, res, next) {
+    databaseConnection("site")
+    .first()
+    .where("url", req.query.site_url)
+    .then(function(site){
+        return databaseConnection("comment").where("site", parseInt(site.id)).orderBy("id", "desc");
+    }).then(function(comments){
+        res.send(comments);
+    });
+});
+
 router.post('/', function(req, res, next) {
-    databaseConnection("comment").insert({
-        message: req.body.message
+    databaseConnection("site")
+    .first()
+    .where("url", req.body.site_url)
+    .then(function(site){
+        if (!site){
+            return databaseConnection("site").insert({
+                url: req.body.site_url
+            }).returning("id");
+        } else {
+            return site.id;
+        }
+    }).then(function(siteId){
+        return databaseConnection("comment").insert({
+            message: req.body.message,
+            site: siteId
+        });
     }).then(function(){
-        res.sendStatus(201);
+        res.render("index");
     });
 });
 
